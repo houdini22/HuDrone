e13fc05b528ae24bbcc635a49bf1901dadf2376b
hud <baniczek@gmail.com>
2019-10-28 09:34:58 +0100

    menu

--- DESKTOP/app/mainwindow.cpp
+++ DESKTOP/app/mainwindow.cpp
@@ -24,11 +24,22 @@ void MainWindow::renderMenu() {
     disconnect(this, SLOT(profileItemTriggered(MyMenuAction*, bool)));
     disconnect(this, SLOT(slotAddProfileTriggered(bool)));
 
-    QAction * addProfileAction = this->menuWidget()->findChild<QAction *>("actionAddProfile");
-    connect(addProfileAction, SIGNAL(triggered(bool)), this, SLOT(slotAddProfileTriggered(bool)));
+    // profiles main menu
+    QMenu * menuProfiles = this->menuWidget()->findChild<QMenu *>("menuProfiles");
+    menuProfiles->clear();
 
-    QMenu * profilesMenu = this->menuWidget()->findChild<QMenu *>("menuSavedProfiles");
-    profilesMenu->clear();
+    // new profile
+    MyMenuAction * menuProfiles_newProfileAction = MyMenuAction::factory(menuProfiles)
+            ->setText("New profile...")
+            ->setParameter("action", "newProfile");
+    connect(menuProfiles_newProfileAction, SIGNAL(myTriggered(MyMenuAction *, bool)), this, SLOT(profileItemTriggered(MyMenuAction *, bool)));
+    menuProfiles->addAction(menuProfiles_newProfileAction);
+
+    menuProfiles->addSeparator();
+
+    QMenu * menuProfiles__profilesAll = new QMenu(menuProfiles);
+    menuProfiles__profilesAll->setTitle("Profiles");
+    menuProfiles->addMenu(menuProfiles__profilesAll);
 
     T_JSON savedProfiles = Config::getInstance().getObject({"profiles"});
 
@@ -60,13 +71,14 @@ void MainWindow::renderMenu() {
         profileMenu->addAction(editAction);
         profileMenu->addAction(deleteAction);
         profileMenu->setTitle(name);
-        profilesMenu->addMenu(profileMenu);
+
+        menuProfiles__profilesAll->addMenu(profileMenu);
     }
 }
 
 void MainWindow::profileItemTriggered(MyMenuAction * action, bool) {
     if (action->getParameter("action").compare("delete") == 0) {
-        if (QMessageBox::question(this, "Confirm", "Are you sure to delete?", QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
+        if (QMessageBox::question(this, "Confirm", "Are you sure to delete profile \"" + action->getParameter("name") + "\"?", QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
             T_JSON config = Config::getInstance().getData();
             T_JSON configProfiles = config["profiles"];
             T_JSON newProfiles = T_JSON::array();
@@ -92,6 +104,8 @@ void MainWindow::profileItemTriggered(MyMenuAction * action, bool) {
         }
     } else if (action->getParameter("action").compare("edit") == 0) {
 
+    } else if (action->getParameter("action").compare("newProfile") == 0) {
+        this->_drone->openWizardAddProfile();
     }
 }
 
