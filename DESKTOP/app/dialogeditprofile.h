233deaf8e15635b3b3248924cef4a84dd5f6e1d7
hud <baniczek@gmail.com>
2019-10-28 09:59:55 +0100

    edit profile

--- DESKTOP/app/dialogeditprofile.h
+++ DESKTOP/app/dialogeditprofile.h
@@ -7,19 +7,7 @@ class DialogEditProfile : public QDialog {
     Q_OBJECT
 public:
     DialogEditProfile(QWidget *parent, QString name);
-
 private:
-    QLabel *label;
-    QLineEdit *lineEdit;
-    QCheckBox *caseCheckBox;
-    QCheckBox *fromStartCheckBox;
-    QCheckBox *wholeWordsCheckBox;
-    QCheckBox *searchSelectionCheckBox;
-    QCheckBox *backwardCheckBox;
-    QDialogButtonBox *buttonBox;
-    QPushButton *findButton;
-    QPushButton *moreButton;
-    QWidget *extension;
 };
 
 #endif // DIALOGEDITPROFILE_H
